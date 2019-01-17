1183f2b5c32bd2a17c763b17dcf7684e58df072c
akcio <akcio@qip.ru>
2019-01-17 11:28:24 +0500

    еьз

--- test.py
+++ test.py
@@ -8,9 +8,6 @@ import numpy
 
 # Grab video from your webcam
 # stream = cv2.VideoCapture(0)
-if not os.path.exists('testVideo/1person.mp4'):
-    print('File not found.')
-    exit(0)
 stream = cv2.VideoCapture('testVideo/1person.mp4')
 # Face detector
 detector = dlib.get_frontal_face_detector()
@@ -98,23 +95,27 @@ def draw_border(img, pt1, pt2, color, thickness, r, d):
 
 
 while True:
+    from datetime import datetime
     # read frames from live web cam stream
     (grabbed, frame) = stream.read()
     
+    startTime = datetime.now()
+    
     # resize the frames to be smaller and switch to gray scale
-    frame = imutils.resize(frame, width=700)
+    #frame = imutils.resize(frame, width=700)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Make copies of the frame for transparency processing
     overlay = frame.copy()
     output = frame.copy()
-
+    endPreparing = datetime.now()
     # set transparency value
     alpha = 0.5
 
     # detect faces in the gray scale frame
     face_rects = detector(gray, 0)
     face_encode = []
+    endSegmentation = datetime.now()
 
     # loop over the face detections
     localDescriptors = descriptors + []
@@ -143,18 +144,26 @@ while True:
         #     if class_length > biggest_class_length:
         #         biggest_class_length = class_length
         # biggest_class = i
-
+        
+        founded = False
         font = cv2.FONT_HERSHEY_DUPLEX
-        if newImageClass < len(descriptorNames):
-            print(descriptorNames[newImageClass])
-            print(i)
-            cv2.putText(overlay, descriptorNames[newImageClass], (d.left() + 6, d.bottom() - 6), font, 1.0, (255, 255, 255), 1)
-        else:
+        for i in range(len(localLabels)-1):
+            if localLabels[i] == newImageClass:
+                founded = True
+                cv2.putText(overlay, descriptorNames[i], (d.left() + 6, d.bottom() - 6), font, 1.0, (255, 255, 255), 1)
+                break
+        if not founded:
             cv2.putText(overlay, "Unknown", (d.left() +6, d.bottom() - 6), font, 1.0, (255,255,255), 1)
+        #if newImageClass < len(descriptorNames):
+        #    print(descriptorNames[newImageClass])
+        #    print(i)
+        #    cv2.putText(overlay, descriptorNames[newImageClass], (d.left() + 6, d.bottom() - 6), font, 1.0, #(255, 255, 255), 1)
+        #else:
+        #    cv2.putText(overlay, "Unknown", (d.left() +6, d.bottom() - 6), font, 1.0, (255,255,255), 1)
 
         # cnt = (cnt+1)%4
 
-
+    endClassification = datetime.now()
     # make semi-transparent bounding box
     cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
 
